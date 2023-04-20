b1402e4909560042cb583b4de23ea6ab246759b0
Laureano Miranda <nmlaureano17@gmail.com>
2023-04-20 17:48:45 -0300

    Segundo commit. agregue ejercicio 3 guia 8 practica

--- 2. Guia Practica/Ejercicio3Guia8Practica/src/ejercicio3guia8practica/Ejercicio3Guia8Practica.java
+++ 2. Guia Practica/Ejercicio3Guia8Practica/src/ejercicio3guia8practica/Ejercicio3Guia8Practica.java
@@ -1,15 +1,78 @@
 package ejercicio3guia8practica;
 
-import java.util.Scanner;
-
+//Crear 4 objetos de tipo Persona con distintos valores, a continuación, llamaremos 
+//todos los métodos para cada objeto, deberá comprobar si la persona está en su peso ideal, 
+//tiene sobrepeso o está por debajo de su peso ideal e indicar para cada objeto si la persona es mayor de edad.
+//Por último, guardaremos los resultados de los métodos calcularIMC y esMayorDeEdad en distintas 
+//variables(arrays), para después calcular un porcentaje de esas 4 personas cuantas están por debajo de su peso, 
+//cuantas en su peso ideal y cuantos, por encima, y también calcularemos un porcentaje de cuantos son mayores de edad y 
+//cuantos menores. Para esto, podemos crear unos métodos adicionales.
 public class Ejercicio3Guia8Practica {
 
-public static void main(String[] args) {
-    
-    Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    public static void main(String[] args) {
+        int[] imc = new int[4];
+        boolean[] edad = new boolean[4];
+        int cont1 = 0, cont2 = 0, cont3 = 0, contMayor = 0, contMenor = 0;
+        
 
+        Persona p1 = new Persona();
+        Persona p2 = new Persona();
+        Persona p3 = new Persona();
+        Persona p4 = new Persona();
+        Servicio sc = new Servicio();
 
+        p1 = sc.crearPersona();
+        edad[0] = sc.esMayor(p1);
+        imc[0] = sc.calcularIMC(p1);
 
+        p2 = sc.crearPersona();
+        edad[1] = sc.esMayor(p2);
+        imc[1] = sc.calcularIMC(p2);
+
+        p3 = sc.crearPersona();
+        edad[2] = sc.esMayor(p3);
+        imc[2] = sc.calcularIMC(p3);
+
+        p4 = sc.crearPersona();
+        edad[3] = sc.esMayor(p4);
+        imc[3] = sc.calcularIMC(p4);
+        
+        for (int i = 0; i < imc.length; i++) {
+            switch (imc[i]) {
+                case -1:
+                    cont1++;
+                    break;
+                case 0:
+                    cont2++;
+                    break;
+                case 1:
+                    cont3++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        double porcentajePesoPorDebajo = ( cont1 * 100 ) / 4;
+        double porcentajePesoIdeal = ( cont2 * 100 ) / 4;
+        double porcentajePesoPorArriba = ( cont3 * 100 ) / 4;
+        
+        System.out.println("Porcentaje peso por debajo: %" + porcentajePesoPorDebajo);
+        System.out.println("Porcentaje peso ideal: %" + porcentajePesoIdeal);
+        System.out.println("Porcentaje sobrepeso: %"+ porcentajePesoPorArriba);
+        
+        for (int i = 0; i < edad.length; i++) {
+            if (edad[i]) {
+                contMayor++;
+            } else{
+                contMenor++;
+            }
+        }
+        double porcentajeMayor = ( contMayor * 100 ) / 4;
+        double porcentajeMenor = ( contMenor * 100 ) / 4;
+        
+        System.out.println("Mayores de edad: %" + porcentajeMayor);
+        System.out.println("Menores de edad: %" + porcentajeMenor);
     }
 
 }
--- 2. Guia Practica/Ejercicio3Guia8Practica/src/ejercicio3guia8practica/Servicio.java
+++ 2. Guia Practica/Ejercicio3Guia8Practica/src/ejercicio3guia8practica/Servicio.java
@@ -12,17 +12,19 @@ public class Servicio {
         x.setNombre(leer.nextLine());
         System.out.println("Ingrese su edad: ");
         x.setEdad(leer.nextInt());
-        while (x.getNombre().toLowerCase() != "h" || x.getNombre().toLowerCase() != "m" || x.getNombre().toLowerCase() != "o") {
+        leer.nextLine();
+        do {
             System.out.println("Ingrese su sexo: ");
             System.out.println("H: Hombre");
             System.out.println("M: Mujer");
             System.out.println("O: Otro");
             x.setSexo(leer.nextLine());
-        }
+        } while (!"h".equalsIgnoreCase(x.getSexo()) && !"m".equalsIgnoreCase(x.getSexo()) && !"o".equalsIgnoreCase(x.getSexo()));
         System.out.println("Ingrese su peso(KG): ");
         x.setPeso(leer.nextDouble());
-        System.out.println("Ingrese su altura(Cms): ");
+        System.out.println("Ingrese su altura(Mts): ");
         x.setAltura(leer.nextDouble());
+        leer.nextLine();
         return x;
     }
 
@@ -33,13 +35,22 @@ public class Servicio {
         }
         return mayor;
     }
-    
-    public double calcularIMC(Persona x) {
-        double pesoIdeal = (x.getPeso()/(x.getAltura()*x.getAltura()));
-        int valid;
-        
-        
-                
+
+    public int calcularIMC(Persona x) {
+        double pesoIdeal = (x.getPeso() / (x.getAltura() * x.getAltura()));
+        int valid = 0;
+
+        if (pesoIdeal < 20) {
+            System.out.println(x.getNombre()+", usted esta por debajo del peso ideal.");
+            valid = -1;
+        } else if (pesoIdeal >= 20 && pesoIdeal <= 25) {
+            System.out.println(x.getNombre()+", usted esta en su peso ideal");
+            valid = 0;
+        } else if (pesoIdeal > 25) {
+            System.out.println(x.getNombre()+", usted tiene sobrepeso.");
+            valid = 1;
+        }
+        return valid;
     }
-    
+
 }
